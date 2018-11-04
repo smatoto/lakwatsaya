f483184c620e12ef57f8456487cb9ba508e4a81b
smatoto <sermil.matoto@gmail.com>
2018-11-04 14:11:32 +0800

    Firebase CD

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code>, save, build and reload.
+            Modify the <code>src/App.js</code> file, save, build and reload.
           </p>
           <a
             className="App-link"
