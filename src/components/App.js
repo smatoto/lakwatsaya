bfc0d60f268ce38efd3ca5df6bc153e9e4ef344a
smatoto <sermil.matoto@gmail.com>
2018-11-06 01:34:33 +0800

    Added Routes

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+import Navigation from './Navigation';
+import LandingPage from './Landing';
+import SignInPage from './SignIn';
+import HomePage from './Home';
+import AccountPage from './Account';
+
+import * as routes from '../constants/routes';
+
+const App = () =>
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr/>
+
+      <Route
+        exact path={routes.LANDING}
+        component={LandingPage}
+      />
+      <Route
+        exact path={routes.SIGN_IN}
+        component={SignInPage}
+      />
+      <Route
+        exact path={routes.HOME}
+        component={HomePage}
+      />
+      <Route
+        exact path={routes.ACCOUNT}
+        component={AccountPage}
+      />
+    </div>
+  </Router>
+
+export default App;
